a3d3fad5529f83798cdd122a2a300b07680392c4
Kuni <121762219+kuneus@users.noreply.github.com>
2024-10-23 21:21:33 -0400

    Create classes and implement hash table

---
+++ .venv/hash_table.py
@@ -0,0 +1,62 @@
+#define hash table here
+#TODO: create hash table
+class HashTable:
+    # constructor with default initial capacity set at 20
+    def __init__(self, initial_capacity=20):
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    # calculate bucket index and create bucket list where item will be inserted
+    def __create_bucket_list(self,key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        return bucket_list
+
+    # insert new item or update its value if it already exists
+    def insert(self, key, item):
+        bucket_list = self.__create_bucket_list(key)
+
+        for kv in bucket_list:
+            if kv[0] == key:
+                kv[1] = item
+                return True
+
+        key_item = [key,item]
+        bucket_list.append(key_item)
+        return True
+
+    # search for an item using a key and return the item if found, or None if not found
+    def search(self,key):
+        bucket_list = self.__create_bucket_list(key)
+
+        for kv in bucket_list:
+            if kv[0] == key:
+                return kv[1]
+
+        return None
+
+    #TODO: remove method
+    def hash_remove(self,key):
+        bucket_list = self.__create_bucket_list(key)
+
+        for kv in bucket_list:
+                if kv[0] == key:
+                    bucket_list.remove(kv)
+                    print('bucket has been removed')
+
+    def print_table(self):
+        print(self.table)
+
+
+exampleTable = HashTable()
+exampleTable.insert('Hello', 'World')
+exampleTable.insert('Hello', 'There')
+exampleTable.insert('Good', 'Bye')
+exampleTable.insert('test delete', 'This')
+print('Before delete: ')
+exampleTable.print_table()
+exampleTable.hash_remove('test delete')
+print('After delete:')
+exampleTable.print_table()
+print(exampleTable.search('Hello'))
