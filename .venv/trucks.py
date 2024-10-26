0309ea6b680c309bd6c6855b0323ad5deb41b50c
Kuni <121762219+kuneus@users.noreply.github.com>
2024-10-25 23:01:10 -0400

    Create constructor and str method

--- .venv/trucks.py
+++ .venv/trucks.py
@@ -1,1 +1,14 @@
 # create truck class here
+class Truck:
+    def __init__(self, capacity, address, packages, depart_time, truck_id):
+        self.capacity = capacity
+        self.speed = 18
+        self.miles = 0
+        self.address = address
+        self.packages = packages
+        self.depart_time = depart_time
+        self.truck_id = truck_id
+
+
+    def __str__(self):
+        return "%s %s %s %s %s %s %s" % (self.capacity, self.speed, self.miles, self.address, self.packages, self.depart_time, self.truck_id)
