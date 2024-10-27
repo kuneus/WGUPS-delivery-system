99948426e1183152aa484c8c9a479495763f1ed4
Kuni <121762219+kuneus@users.noreply.github.com>
2024-10-27 13:53:09 -0400

    Update truck attributes

--- .venv/trucks.py
+++ .venv/trucks.py
@@ -1,11 +1,12 @@
 # create truck class here
 class Truck:
-    def __init__(self, capacity, address, packages, depart_time, truck_id):
-        self.capacity = capacity
+    def __init__(self, address, packages, depart_time, truck_id):
+        self.capacity = 16
         self.speed = 18
-        self.miles = 0
+        self.miles = 0.0
         self.address = address
-        self.packages = packages
+        self.to_deliver = packages
+        self.delivered = []
         self.depart_time = depart_time
         self.truck_id = truck_id
 
