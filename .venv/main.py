bf49cd32a02569589e6c222fc706cad1344155eb
Kuni <121762219+kuneus@users.noreply.github.com>
2024-10-27 13:09:49 -0400

    Add self-identifying comments at top of document

--- .venv/main.py
+++ .venv/main.py
@@ -1,3 +1,6 @@
+# Name: Kunikazu Nishimura
+# Student ID:  011703011
+
 import csv
 from package import load_package_data
 from hash_table import HashTable
@@ -7,13 +10,14 @@ from distance import *
 package_hash_table = HashTable()
 load_package_data('./csv/wgups-packages.csv', package_hash_table)
 package_hash_table.print_table()
+print(package_hash_table.lookup('32'))
 
 # create hash table for distances and load distance data
 address_hash_table = HashTable()
 load_distance_data('./csv/wgpus-distances.csv', address_hash_table)
 
 # test address data and functions
-address_1 = address_hash_table.search('4001 South 700 East')
-address_2 = address_hash_table.search('1330 2100 S')
+address_1 = address_hash_table.lookup('4001 South 700 East')
+address_2 = address_hash_table.lookup('1330 2100 S')
 
 print(find_min_distance(address_1))
