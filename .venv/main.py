ea5250c22bdabb0284d7af97cf34f72c8d1ccc03
Kuni <121762219+kuneus@users.noreply.github.com>
2024-10-26 23:20:17 -0400

    Add test code for testing packages, distance data, and functions

--- .venv/main.py
+++ .venv/main.py
@@ -1,1 +1,19 @@
-print('Hello world!')
+import csv
+from package import load_package_data
+from hash_table import HashTable
+from distance import *
+
+# create hash table for packages and load packages data
+package_hash_table = HashTable()
+load_package_data('./csv/wgups-packages.csv', package_hash_table)
+package_hash_table.print_table()
+
+# create hash table for distances and load distance data
+address_hash_table = HashTable()
+load_distance_data('./csv/wgpus-distances.csv', address_hash_table)
+
+# test address data and functions
+address_1 = address_hash_table.search('4001 South 700 East')
+address_2 = address_hash_table.search('1330 2100 S')
+
+print(find_min_distance(address_1))
