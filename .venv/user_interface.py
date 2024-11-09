f096177a1c6709d97fe5056112015def21f06407
Kuni <121762219+kuneus@users.noreply.github.com>
2024-11-08 21:16:54 -0500

    Move code block for displaying single package status into separate function

--- .venv/user_interface.py
+++ .venv/user_interface.py
@@ -3,16 +3,6 @@ from datetime import datetime
 from package import package_hash_table
 from trucks import truck_hash_table
 
-# user must be able to
-# 1. view the delivery status of any package at any time
-# 2. view the total mileage traveled by all trucks
-
-# main function should act as a loop until the user quits
-# options:
-#   view delivery status of specific or all packages
-#   view mileage of all trucks
-#   exit program
-
 # check if time input is valid
 def validate_time(time_str, time_format):
     try:
@@ -30,6 +20,27 @@ def calculate_package_status(truck_obj, pkg_obj, time_input):
     else:
         pkg_obj.status = 'At hub'
     return pkg_obj.status
+
+# display status of a single package
+def display_single_package(time_obj):
+    # run do-while loop until user inputs valid package ID
+    print('Please input the package ID: ')
+    while True:
+        package_id = input()
+        pkg_obj = package_hash_table.lookup(package_id)
+        if pkg_obj:
+            break
+        else:
+            print('Invalid package ID. Please try again with a valid package ID.')
+
+    # find which truck the package will be delivered in
+    pkg_truck_id = pkg_obj.truck_id
+    truck_obj = truck_hash_table.lookup(pkg_truck_id)
+
+    # determine package status by calculating delivery time and comparing it to input time
+    status = calculate_package_status(truck_obj, pkg_obj, time_obj)
+    time_str = time_obj.strftime('%I:%M %p')
+    print('At %s, package %s status: %s' % (time_str, pkg_obj.package_id, status))
 
 # display status of all packages in each truck
 def display_all_statuses(trucks, time_input):
@@ -54,13 +65,13 @@ def display_all_mileage(trucks):
         print('%s ------------ %.1f' % (truck.truck_id, truck.miles))
     print('Total miles: %.1f' % total_miles)
 
-
 def user_interface( trucks):
     # welcome message
     print('---------------------------------------------------------------')
     print('------------------     Welcome to WGUPS!     ------------------')
     print('---------------------------------------------------------------')
 
+    # run loop of user interface program until user quits
     while True:
         response = input("Please input one of the following options: \n"
                          " '1' to view the status of a specific package, \n "
@@ -68,6 +79,7 @@ def user_interface( trucks):
                          " '3' to view total miles by all trucks, \n"
                          "  or 'q' to quit the program:  ")
 
+        # quit program
         if response == 'q':
             break
 
@@ -82,25 +94,9 @@ def user_interface( trucks):
             else:
                 print('Invalid time format.')
 
+        # view status of a specific package
         if response == '1':
-            # run do-while loop until user inputs valid package ID
-            print('Please input the package ID: ')
-            while True:
-                package_id = input()
-                pkg_obj = package_hash_table.lookup(package_id)
-                if pkg_obj:
-                    break
-                else:
-                    print('Invalid package ID. Please try again with a valid package ID.')
-
-            # find which truck the package will be delivered in
-            pkg_truck_id = pkg_obj.truck_id
-            truck_obj = truck_hash_table.lookup(pkg_truck_id)
-
-            # determine package status by calculating delivery time and comparing it to input time
-            status = calculate_package_status(truck_obj,pkg_obj,current_time_obj)
-            time_str = current_time_obj.strftime('%I:%M %p')
-            print('At %s, package %s status: %s' % (time_str, pkg_obj.package_id, status))
+            display_single_package(current_time_obj)
         elif response == '2':
             # display status of all packages in each truck
             display_all_statuses(trucks, current_time_obj)
